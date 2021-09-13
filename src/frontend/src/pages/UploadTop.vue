702f8b47f1e2779cea2739467b163d5a139a843f
mizuki-n-2 <mizukizhangze@gmail.com>
2021-09-13 15:30:26 +0900

    UIの微調整

--- src/frontend/src/pages/UploadTop.vue
+++ src/frontend/src/pages/UploadTop.vue
@@ -9,33 +9,40 @@
     >
       <v-list-item>
         <v-list-item-content>
-          <v-list-item-title class="text-h5 my-8 text-center">
+          <v-list-item-title class="text-h5 my-5 text-center">
             epubデータのアップロード
           </v-list-item-title>
-          <v-list-item-subtitle>以下の情報を入力してください。</v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
 
-      <v-list-item>
-        <v-list-item-content>
-          <label for="file" class="mb-2 font-weight-bold">epubデータ</label>
-          <input type="file" id="file">
-        </v-list-item-content>
-      </v-list-item>
+      <v-list-item-group class="px-15">
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-subtitle>以下の情報を入力してください。</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
 
-      <v-list-item>
-        <v-list-item-content>
-          <label for="price" class="mb-2 font-weight-bold">本の値段</label>
-          <span><input type="number" id="price"> 円</span>
-        </v-list-item-content>
-      </v-list-item>
+        <v-list-item class="mb-8">
+          <v-list-item-content>
+            <label for="file" class="mb-2 font-weight-bold">epubデータ</label>
+            <input type="file" id="file">
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item class="mb-8">
+          <v-list-item-content>
+            <label for="price" class="mb-2 font-weight-bold">本の値段</label>
+            <span><input type="number" id="price"> 円</span>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
 
       <v-card-actions>
         <v-btn
           rounded
           color="orange"
           dark
-          class="mx-auto my-5"
+          class="mx-auto mb-5"
         >
           アップロード
         </v-btn>
