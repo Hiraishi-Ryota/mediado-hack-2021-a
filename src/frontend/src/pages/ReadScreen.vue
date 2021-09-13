86a6ebea081973dd887af7c51b1e6649dc4f6a30
maru0pengin <maru0pengin@gmail.com>
2021-09-13 16:46:00 +0900

    update:webにBibiを埋め込み

--- src/frontend/src/pages/ReadScreen.vue
+++ src/frontend/src/pages/ReadScreen.vue
@@ -1,16 +1,33 @@
 <template>
-  <div>
-    本を読む画面
-    <a
-      href="http://localhost:8000/static/bibi/index.html?book=test.epub"
-      data-bibi="embed"
-      data-bibi-style="width: 100%; height: 400px;"
-      >マイ・グレート・コミック</a
-    >
-    <!-- <script src="http://localhost:8000/static/bibi/and/jo.js"></script> -->
+  <div class="main">
+    <div class="fullheight">
+      <a
+        href="http://localhost:8000/static/bibi/index.html?book=test.epub"
+        data-bibi="embed"
+        data-bibi-style="width: 100%; height: 1000px;"
+        >マイ・グレート・コミック</a
+      >
+      <script2 src="http://localhost:8000/static/bibi/and/jo.js"></script2>
+      <!-- <script src="http://localhost:8000/static/bibi/and/jo.js"></script> -->
+    </div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import VS2 from 'vue-script2'
+
+Vue.use(VS2)
 export default {}
 </script>
+
+<style scoped>
+main {
+  height: 100%;
+  margin: 0;
+}
+
+.fullheight {
+  height: 100%;
+}
+</style>
