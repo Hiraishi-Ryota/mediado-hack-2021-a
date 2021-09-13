dfe3496e8d3ff6d766b556f2742e77921ca7ba1c
mizuki-n-2 <mizukizhangze@gmail.com>
2021-09-13 15:45:53 +0900

    アップロード画面のUI微調整

--- src/frontend/src/pages/UploadTop.vue
+++ src/frontend/src/pages/UploadTop.vue
@@ -15,7 +15,7 @@
         </v-list-item-content>
       </v-list-item>
 
-      <v-list-item-group class="px-15">
+      <div class="px-15">
         <v-list-item>
           <v-list-item-content>
             <v-list-item-subtitle>以下の情報を入力してください。</v-list-item-subtitle>
@@ -35,7 +35,7 @@
             <span><input type="number" id="price"> 円</span>
           </v-list-item-content>
         </v-list-item>
-      </v-list-item-group>
+      </div>
 
       <v-card-actions>
         <v-btn
