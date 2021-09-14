fea7ec60e6d6847f56421a5af48612c9812701f5
leonard475192 <leonard.t1028@gmail.com>
2021-09-14 13:50:39 +0900

    add:CORSMiddleware

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 from books.router import router as book_router
@@ -8,6 +9,14 @@ from db import Base, engine
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # books routerを登録する。
 app.include_router(book_router)
