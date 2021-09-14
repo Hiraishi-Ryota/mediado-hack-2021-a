966f45516de60a69f1e6d6c0e6d6501695c5f5d4
maru0pengin <maru0pengin@gmail.com>
2021-09-14 11:42:46 +0900

    update:本詳細のuiを作成

--- src/frontend/src/pages/BookDetails.vue
+++ src/frontend/src/pages/BookDetails.vue
@@ -1,23 +1,112 @@
 <template>
   <div>
-    本詳細画面
-    <v-btn elevation="2" :ripple="false" @click="gotoRead()"
-      >この章を読む</v-btn
-    >
+    <Header :color="color"></Header>
+    <v-container>
+      <v-col>
+        <h1 class="text-center mt-8">{{ bookDetail.title }}</h1>
+      </v-col>
+      <v-row>
+        <v-col cols="6" class="d-flex justify-center align-center">
+          <v-img :src="bookDetail.cover_img" max-width="200" class="mx-auto">
+          </v-img>
+        </v-col>
+        <v-col cols="4" class="d-flex justify-center align-center">
+          <h2>
+            著者:{{ bookDetail.author }}
+            <p />
+            値段:{{ bookDetail.price }}
+            <p />
+            文字数:{{ bookDetail.word_count }}
+          </h2>
+        </v-col>
+      </v-row>
+      <v-row
+        v-for="(chapter, index) in bookDetail.chapters"
+        :key="chapter.id"
+        class="mt-10"
+      >
+        <v-col cols="2" class="d-flex justify-center align-center">
+          <h1 class="font-weight-bold">{{ index + 1 }}章</h1>
+        </v-col>
+        <v-col cols="6" class="d-flex justify-center align-center">
+          <h1>{{ chapter.title }}</h1>
+        </v-col>
+        <v-col cols="2" class="d-flex justify-center align-center">
+          <h1>{{ chapter.price }}円</h1>
+        </v-col>
+        <v-col cols="2" class="d-flex justify-center align-center">
+          <v-btn elevation="2" :ripple="false" @click="gotoRead(chapter.e_pub)"
+            >読む</v-btn
+          >
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
+import Header from '../components/Header.vue'
 export default {
+  components: {
+    Header,
+  },
   data() {
     return {
-      title: 'test',
+      color: 'primary',
+      bookDetail: {
+        id: 1,
+        title: 'ワンピース',
+        price: 3000,
+        author: '尾田栄一郎',
+        cover_img:
+          'https://images-na.ssl-images-amazon.com/images/I/812NbfCwTvL.jpg',
+        e_pub: 'ワンピース',
+        word_count: 1000,
+        chapters: [
+          {
+            title: '1章のタイトル',
+            price: 600,
+            word_count: 100,
+            e_pub: 'test',
+            author: '尾田栄一郎',
+            id: 1,
+            book_id: 1,
+          },
+          {
+            title: '2章のタイトル',
+            price: 700,
+            word_count: 100,
+            e_pub: 'test',
+            author: '尾田栄一郎',
+            id: 2,
+            book_id: 1,
+          },
+          {
+            title: '3章のタイトル',
+            price: 800,
+            word_count: 100,
+            e_pub: 'test',
+            author: '尾田栄一郎',
+            id: 3,
+            book_id: 1,
+          },
+          {
+            title: '4章のタイトル',
+            price: 900,
+            word_count: 100,
+            e_pub: 'test',
+            author: '尾田栄一郎',
+            id: 4,
+            book_id: 1,
+          },
+        ],
+      },
     }
   },
   methods: {
-    gotoRead: function() {
+    gotoRead: function(epub) {
       this.$router.push({
-        path: `/read_screen/${this.$route.params['id']}/${this.title}`,
+        path: `/read_screen/${this.$route.params['id']}/${epub}`,
       })
     },
   },
